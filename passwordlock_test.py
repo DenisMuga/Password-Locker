3a29a4f12277031ecb60290a1f9df1169f49091d
DenisMuga <denismugah5@gmail.com>
2022-04-24 13:33:03 +0300

    creating a test class for various User Class test cases

--- passwordlock_test.py
+++ passwordlock_test.py
@@ -1,3 +1,8 @@
 import unittest
 from passwdlock import Users
 from passwdlock import Credentials
+
+class TestClass(unittest.TestCase):
+    """
+    A test class for various User Class test cases
+    """
